f416231ad5408b9684858744471146cd9d26e6ba
Astrokojo <astrokojo@gmail.com>
2023-01-06 16:31:16 +0000

    Added models to parameter

--- src/main/java/com/kaos/FleetMS/ApplicationController.java
+++ src/main/java/com/kaos/FleetMS/ApplicationController.java
@@ -9,8 +9,34 @@ public class ApplicationController {
     public String home(){
         return "index";
     }
-    @GetMapping("/users-profile")
-    public String widget(){
-        return "users-profile";
+    @GetMapping("/layout")
+    public String layout(){
+        return "layout";
+    }
+
+    @GetMapping("/accounts")
+    public String accountsDept() {
+        return "/accounts/index";
+    }
+    @GetMapping("/fleet")
+    public String fleetDept() {
+    return "/fleet/index";
+    }
+    @GetMapping("/helpdesk")
+    public String helpdeskDept(){
+        return "/helpdesk/index";
+    }
+
+    @GetMapping("/hr")
+    public String hrDept(){
+        return "/hr/index";
+    }
+    @GetMapping("/parameters")
+    public String parametersDept(){
+        return "/parameters/index";
+    }
+    @GetMapping("/payroll")
+    public String payrollDept(){
+        return "/payroll/index";
     }
 }
